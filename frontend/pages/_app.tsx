51eb441dcbfbcf9b2c23dd4101dce7dd97326603
oemerfurkan <omerfurkanyuruk1@gmail.com>
2023-08-12 16:22:09 +0300

    verify functionality

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,13 +4,48 @@ import type { AppProps } from "next/app";
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
+import { Chain, configureChains, createConfig, WagmiConfig } from "wagmi";
+import { optimismGoerli } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
+const opstack: Chain = {
+  id: 42069,
+  name: "op stack",
+  network: "opstack",
+  nativeCurrency: {
+      name: "Goerli Ether",
+      symbol: "ETH",
+      decimals: 18,
+  },
+  rpcUrls: {
+      default: {
+          http: ["http://localhost:8545"],
+      },
+      public: {
+          http: ["http://localhost:8545"]
+      },
+  },
+  blockExplorers: {
+      etherscan: {
+          name: "Etherscan",
+          url: "https://goerli-optimism.etherscan.io",
+      },
+      default: {
+          name: "Etherscan",
+          url: "https://goerli-optimism.etherscan.io",
+      },
+  },
+  contracts: {
+      multicall3: {
+          address: "0xF568128e4a37B0D1E439e3c075bC24582850ABB0",
+      },
+  },
+  testnet: true,
+};
+
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, zora],
+  [optimismGoerli, opstack],
   [publicProvider()]
 );
 
--- frontend/pages/verify.tsx
+++ frontend/pages/verify.tsx
@@ -5,15 +5,51 @@ import Link from "next/link";
 import { Heading } from "@/components/Heading";
 import Head from "next/head";
 
+import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
+import { Abi, Narrow } from "viem";
+import { optimismGoerli } from "wagmi/chains";
+import { useEffect } from "react";
+
 const passionOne = Passion_One({ weight: "400", subsets: ["latin"] });
 
+const l1contractAddress = "7c45E0ed874609Edb2797f224f725A887250C15B"
+
 export default function Home() {
+
+    const { address } = useAccount()
+
+    const { config, error } = usePrepareContractWrite({
+        address: `0x${l1contractAddress}`,
+        abi: [{
+            "name": "sendTestMessages",
+            "type": "function",
+            "stateMutability": "nonpayable",
+            "inputs": [],
+            "outputs": [],
+        }] as const,
+        functionName: "sendTestMessages",
+        chainId: optimismGoerli.id
+    })
+
+    const { data, isLoading, isSuccess, write: verifyAddress } = useContractWrite( config )
+
+    useEffect(() => {
+        if (isSuccess) {
+            window.location.href = "/verified"
+        }
+    }, [isSuccess])
+
   return (
     <>
       <div
         className={`h-screen flex flex-col gap-32 justify-center items-center ${passionOne.className}`}
       >
-        <button className="connect">VERIFY</button>
+        {isLoading 
+        ? 
+        <button className="connect">VERIFYING...</button>
+        :
+        <button onClick={() => verifyAddress?.()} className="connect">VERIFY</button>}
+        
         <p className="w-1/3 text-3xl font-thin">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore fuga ducimus rem quia suscipit minima illum corporis sint, quos, sit dolorem ex quis ut animi quae iure! Deserunt incidunt odio facilis esse voluptatem inventore quas ducimus, vitae eius rerum facere nulla modi rem nesciunt laborum laudantium reprehenderit debitis culpa vel.
         </p>
